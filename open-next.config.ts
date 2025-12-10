4a7b35d2a875c87a37365a8c45771bf60411fe6e
dansarpong <dansarpong.me@gmail.com>
2025-12-10 08:45:43 +0000

    fix: Cloudflare integration

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+
+export default defineCloudflareConfig({
+    // Configuration can be added here as needed
+    // For example: incrementalCache: r2IncrementalCache
+});
